c3fbe074b4f725e3da1cbf1231aeb62636b3d135
Vincent Bernardoff <vb@luminar.eu.org>
2017-03-28 15:53:47 +0200

    script: fix bs-devkit

--- scripts/install_bmex.sh
+++ scripts/install_bmex.sh
@@ -9,7 +9,7 @@ opam pin add -n -y --dev-repo conduit
 opam pin add -n -y nocrypto git://github.com/vbmithr/ocaml-nocrypto#sexp-dev
 opam pin add -n -y cohttp git://github.com/vbmithr/ocaml-cohttp#sexp-dev
 opam pin add -n -y dtc git://github.com/vbmithr/ocaml-dtc
-opam pin add -n -y bs-devkit git://github.com/vbmithr/bs_devkit
+opam pin add -n -y bs-devkit git://github.com/vbmithr/bs_devkit#no-bin-prot
 opam pin add -n -y bs-api git://github.com/vbmithr/bs_api
 opam pin add -n -y bmex git://github.com/vbmithr/bmex
 
