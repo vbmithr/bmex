3becc46fb1ab682a2dc9762bac1d2016a3a7d283
Vincent Bernardoff <vb@luminar.eu.org>
2017-08-11 10:05:53 +0200

    add ExecInst.Close on close orders

--- src/bitmex_prod.ml
+++ src/bitmex_prod.ml
@@ -1414,13 +1414,18 @@ let submit_order w ~key ~secret (req : DTC.Submit_new_single_order.t) stop_exec_
   let timeInForce = Option.value ~default:`tif_unset req.time_in_force in
   let price, stopPx =
     OrderType.to_price_stopPx ?p1:req.price1 ?p2:req.price2 ordType in
-  let stop_exec_inst = match ordType with
+  let execInst =
+    match Option.value ~default:`trade_unset req.open_or_close with
+    | `trade_close -> [ ExecInst.Close ]
+    | `trade_open
+    | `trade_unset -> [] in
+  let execInst = match ordType with
     | `order_type_market
-    | `order_type_limit -> []
-    | #OrderType.t -> [stop_exec_inst] in
+    | `order_type_limit -> execInst
+    | #OrderType.t -> stop_exec_inst :: execInst in
   let displayQty, execInst = match timeInForce with
-    | `tif_all_or_none -> Some 0, ExecInst.AllOrNone :: stop_exec_inst
-    | #DTC.time_in_force_enum -> None, stop_exec_inst in
+    | `tif_all_or_none -> Some 0, ExecInst.AllOrNone :: execInst
+    | #DTC.time_in_force_enum -> None, execInst in
   let order =
     REST.Order.create
       ?displayQty
